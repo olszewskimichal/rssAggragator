b770c534b34098726546b9d7af1dd918543cfcb2
Michał <michalolsz@mat.umk.pl>
2017-10-27 17:56:20 +0200

    init projektu

---
+++ src/test/java/pl/michal/olszewski/rssaggregator/RssAggregatorApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.michal.olszewski.rssaggregator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RssAggregatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
