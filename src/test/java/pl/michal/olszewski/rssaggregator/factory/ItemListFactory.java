4a86372268085355b30c3d4ced89b1ea7a473b76
Michał <olszewskimichal@outlook.com>
2017-11-04 18:08:14 +0100

    TDD - faza red - API do najswiezszej ilosci itemów

---
+++ src/test/java/pl/michal/olszewski/rssaggregator/factory/ItemListFactory.java
@@ -0,0 +1,22 @@
+package pl.michal.olszewski.rssaggregator.factory;
+
+import java.util.stream.IntStream;
+import pl.michal.olszewski.rssaggregator.dto.ItemDTO;
+import pl.michal.olszewski.rssaggregator.entity.Blog;
+import pl.michal.olszewski.rssaggregator.entity.Item;
+import pl.michal.olszewski.rssaggregator.repository.BlogRepository;
+
+public class ItemListFactory {
+
+  private final BlogRepository repository;
+
+  public ItemListFactory(BlogRepository repository) {
+    this.repository = repository;
+  }
+
+  public void buildNumberOfItemsAndSave(int numberOfItems) {
+    Blog blog = new Blog("blog997", "", "", "", null);
+    IntStream.rangeClosed(1, numberOfItems).forEach(v -> blog.addItem(new Item(ItemDTO.builder().title("title" + v).build())));
+    repository.save(blog);
+  }
+}
---
+++ src/test/java/pl/michal/olszewski/rssaggregator/integration/NewestItemsApiTest.java
@@ -0,0 +1,63 @@
+package pl.michal.olszewski.rssaggregator.integration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import pl.michal.olszewski.rssaggregator.assertion.ItemListAssert;
+import pl.michal.olszewski.rssaggregator.dto.ItemDTO;
+import pl.michal.olszewski.rssaggregator.factory.ItemListFactory;
+import pl.michal.olszewski.rssaggregator.repository.BlogRepository;
+
+public class NewestItemsApiTest extends IntegrationTest {
+
+  @Autowired
+  private BlogRepository repository;
+
+  @Test
+  public void should_get_empty_list_of_items() {
+    givenItem()
+        .buildNumberOfItemsAndSave(0);
+
+    List<ItemDTO> items = thenGetItemsFromApi();
+
+    assertThat(items).isEmpty();
+  }
+
+  @Test
+  public void should_get_all_items() {
+    givenItem()
+        .buildNumberOfItemsAndSave(3);
+
+    List<ItemDTO> items = thenGetItemsFromApi();
+
+    assertThat(items).hasSize(3);
+  }
+
+  @Test
+  public void should_get_limit_three_items() {
+    givenItem()
+        .buildNumberOfItemsAndSave(6);
+
+    List<ItemDTO> itemDefinitionDTOS = thenGetNumberItemsFromApi(3);
+
+    ItemListAssert.assertThat(itemDefinitionDTOS)
+        .isSuccessful()
+        .hasNumberOfItems(3);
+  }
+
+  private ItemListFactory givenItem() {
+    return new ItemListFactory(repository);
+  }
+
+  private List<ItemDTO> thenGetItemsFromApi() {
+    return Arrays.asList(template.getForEntity(String.format("http://localhost:%s/api/v1/items", port), ItemDTO[].class).getBody());
+  }
+
+  private List<ItemDTO> thenGetNumberItemsFromApi(int number) {
+    return Arrays.asList(template.getForEntity(String.format("http://localhost:%s/api/v1/items?limit=%s", port, number), ItemDTO[].class).getBody());
+  }
+
+}
