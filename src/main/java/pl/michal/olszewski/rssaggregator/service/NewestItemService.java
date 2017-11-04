faca4952d51751b177ac5746a2f757dcf072aec2
Michał <olszewskimichal@outlook.com>
2017-11-04 17:43:31 +0100

    TDD - faza green - serwis do najswiezszej ilosci itemów

--- src/main/java/pl/michal/olszewski/rssaggregator/service/NewestItemService.java
+++ src/main/java/pl/michal/olszewski/rssaggregator/service/NewestItemService.java
@@ -1,6 +1,7 @@
 package pl.michal.olszewski.rssaggregator.service;
 
 import java.util.List;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import pl.michal.olszewski.rssaggregator.entity.Item;
 import pl.michal.olszewski.rssaggregator.repository.ItemRepository;
@@ -15,6 +16,6 @@ public class NewestItemService {
   }
 
   public List<Item> getNewestItems(int size) {
-    return null;
+    return itemRepository.findAllByOrderByDateDesc(new PageRequest(0, size)).getContent();
   }
 }
