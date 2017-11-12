bd79f54adbaa565637d3d8b6406369d487013b39
Michał <olszewskimichal@outlook.com>
2017-11-12 12:31:12 +0100

    obsługa uruchamiania aplikacji jako usługi z kodowaniem UTF-8

--- src/main/java/pl/michal/olszewski/rssaggregator/RssAggregatorApplication.java
+++ src/main/java/pl/michal/olszewski/rssaggregator/RssAggregatorApplication.java
@@ -4,12 +4,14 @@ import java.util.concurrent.Executor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication
 @EnableCaching
@@ -29,5 +31,15 @@ public class RssAggregatorApplication {
     ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
     threadPoolTaskExecutor.setCorePoolSize(8);
     return threadPoolTaskExecutor;
+  }
+
+  @Bean
+  public FilterRegistrationBean filterRegistrationBean() {
+    FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+    CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+    characterEncodingFilter.setForceEncoding(true);
+    characterEncodingFilter.setEncoding("UTF-8");
+    registrationBean.setFilter(characterEncodingFilter);
+    return registrationBean;
   }
 }
