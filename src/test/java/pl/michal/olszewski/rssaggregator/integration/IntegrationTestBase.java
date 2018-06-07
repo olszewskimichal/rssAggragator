2ee7222c327cbbe1d7e14ffab826902d8a650699
Michał <olszewskimichal@outlook.com>
2018-06-07 20:58:04 +0200

    Podniesienie wersji springa do 2.0.2

--- src/test/java/pl/michal/olszewski/rssaggregator/integration/IntegrationTestBase.java
+++ src/test/java/pl/michal/olszewski/rssaggregator/integration/IntegrationTestBase.java
@@ -1,18 +1,16 @@
 package pl.michal.olszewski.rssaggregator.integration;
 
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import pl.michal.olszewski.rssaggregator.config.Profiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles(Profiles.TEST)
-@RunWith(JUnitPlatform.class)
-@ExtendWith(org.springframework.test.context.junit.jupiter.SpringExtension.class)
+@ExtendWith(SpringExtension.class)
 public abstract class IntegrationTestBase {
 
   final TestRestTemplate template = new TestRestTemplate();
