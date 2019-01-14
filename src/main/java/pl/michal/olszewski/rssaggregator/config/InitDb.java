4dadea5f12dbcefa2cc3c7f3683bff20bc0d9641
olszewskimichal <olszewskimichal@outlook.com>
2019-01-14 21:44:16 +0100

    remove comment

--- src/main/java/pl/michal/olszewski/rssaggregator/config/InitDb.java
+++ src/main/java/pl/michal/olszewski/rssaggregator/config/InitDb.java
@@ -11,25 +11,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Profile({Profiles.DEVELOPMENT, Profiles.PRODUCTION})
 public class InitDb {
-
-/*  private final BlogRepository blogRepository;
-
-  public InitDb(BlogRepository blogRepository) {
-    this.blogRepository = blogRepository;
-  }
-
-  //@PostConstruct
-  public void populateDatabase() {
-    Stream.of("https://devstyle.pl", "https://vladmihalcea.com", "http://jakoszczedzacpieniadze.pl", "https://kobietydokodu.pl", "https://codecouple.pl", "https://geekyprimitives.wordpress.com/",
-        "https://aboullaite.me/", "http://www.javadevmatt.pl/")
-        .map(v -> new Blog(v, "", "", v + "/feed", null, null))
-        .forEach(blogRepository::save);
-    blogRepository.save(new Blog("http://jvm-bloggers.com", "", "JVM-Bloggers", "http://jvm-bloggers.com/pl/rss", null, null));
-    blogRepository.save(new Blog("https://programistanaswoim.pl", "", "ProgramistaNaSwoim", "http://feeds.feedburner.com/ProgramistaNaSwoim", null, null));
-    blogRepository.save(new Blog("http://blog.codeleak.pl/", "", "Codeleak.pl", "http://blog.codeleak.pl/feeds/posts/default?alt=rss", null, null));
-    blogRepository.save(new Blog("https://spring.io/blog", "", "Spring", "https://spring.io/blog.atom/", null, null));
-  }*/
-
+  
   @Bean
   public TaskScheduler taskScheduler() {
     return new ConcurrentTaskScheduler();
