c62b8cae8d4e7d9b5e65a6f43682e7455b7da1fc
olszewskimichal <olszewskimichal@outlook.com>
2019-01-17 22:09:42 +0100

    refactor

--- src/main/java/pl/michal/olszewski/rssaggregator/RssAggregatorApplication.java
+++ src/main/java/pl/michal/olszewski/rssaggregator/RssAggregatorApplication.java
@@ -7,7 +7,6 @@ import java.util.concurrent.Executor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
@@ -15,7 +14,6 @@ import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.web.filter.CharacterEncodingFilter;
 import pl.michal.olszewski.rssaggregator.config.Profiles;
 
 @SpringBootApplication
--- src/main/java/pl/michal/olszewski/rssaggregator/controller/HomeController.java
+++ src/main/java/pl/michal/olszewski/rssaggregator/controller/HomeController.java
@@ -3,7 +3,6 @@ package pl.michal.olszewski.rssaggregator.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
