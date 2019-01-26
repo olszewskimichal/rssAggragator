f4f96be72ab533a10da5b22f5f542846f3e1999e
Michal Olszewski <michal.a.olszewski@allegro.pl>
2019-01-26 20:27:27 +0100

    refactor - remove block from updateBlogService

--- src/main/java/pl/michal/olszewski/rssaggregator/blog/UpdateBlogService.java
+++ src/main/java/pl/michal/olszewski/rssaggregator/blog/UpdateBlogService.java
@@ -1,54 +1,69 @@
 package pl.michal.olszewski.rssaggregator.blog;
 
-import java.util.List;
-import java.util.concurrent.Executor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
+
+import java.util.List;
+import java.util.concurrent.Executor;
 
 @Service
 @Slf4j
 @Transactional
 class UpdateBlogService {
 
-  private final BlogReactiveRepository repository;
-  private final AsyncService asyncService;
-  private final Executor executor;
+    private final BlogReactiveRepository repository;
+    private final AsyncService asyncService;
+    private final Executor executor;
 
-  @Value("${refresh.blog.enable-job}")
-  private boolean enableJob;
+    @Value("${refresh.blog.enable-job}")
+    private boolean enableJob;
 
-  public UpdateBlogService(BlogReactiveRepository repository, AsyncService asyncService, Executor executor) {
-    this.repository = repository;
-    this.asyncService = asyncService;
-    this.executor = executor;
-  }
-
-  @Scheduled(fixedDelayString = "${refresh.blog.milis}")
-  public void updatesBlogs() {
-    if (enableJob) {
-      log.debug("zaczynam aktualizacje blogów");
-      Mono<List<Boolean>> collect = repository.findAll()
-          .flatMap(v -> Mono.fromCallable(() -> asyncService.updateBlog(v)).subscribeOn(Schedulers.fromExecutor(executor)).onErrorReturn(false)) //TODO skrocic linie
-          .collectList();
-      Flux.merge(collect)
-          .subscribeOn(Schedulers.parallel())
-          .collectList().block(); //TODO usunac blocka
-      log.debug("Aktualizacja zakończona");
+    public UpdateBlogService(BlogReactiveRepository repository, AsyncService asyncService, Executor executor) {
+        this.repository = repository;
+        this.asyncService = asyncService;
+        this.executor = executor;
     }
-  }
 
-  public void refreshBlogFromId(String id) {
-    log.debug("Odswiezam bloga o id {}", id);
-    repository.findById(id)
-        .switchIfEmpty(Mono.error(new BlogNotFoundException(id)))
-        .map(asyncService::updateBlog)
-        .subscribe();
-  }
+    @Scheduled(fixedDelayString = "${refresh.blog.milis}")
+    Disposable runScheduledUpdate() {
+        return updateAllActiveBlogs().subscribe();
+    }
+
+    Mono<List<List<Boolean>>> updateAllActiveBlogs() {
+        if (enableJob) {
+            log.debug("zaczynam aktualizacje blogów");
+            Mono<List<Boolean>> collect = repository.findAll()
+                .flatMap(this::updateBlog)
+                .collectList();
+            return Flux.merge(collect)
+                .subscribeOn(Schedulers.parallel())
+                .collectList()
+                .doOnSuccess(v -> log.debug("Aktualizacja zakonczona"));
+        }
+        return Mono.empty();
+    }
+
+    private Mono<Boolean> updateBlog(Blog blog) {
+        log.error("update blog {}", blog);
+        return Mono.fromCallable(() -> asyncService.updateBlog(blog))
+            .subscribeOn(Schedulers.fromExecutor(executor))
+            .doOnNext(v -> log.error("Przetworzylem {}", v))
+            .onErrorReturn(false);
+    }
+
+    public void refreshBlogFromId(String id) {
+        log.debug("Odswiezam bloga o id {}", id);
+        repository.findById(id)
+            .switchIfEmpty(Mono.error(new BlogNotFoundException(id)))
+            .map(asyncService::updateBlog)
+            .subscribe();
+    }
 
 }
--- src/test/java/pl/michal/olszewski/rssaggregator/blog/UpdateBlogServiceTest.java
+++ src/test/java/pl/michal/olszewski/rssaggregator/blog/UpdateBlogServiceTest.java
@@ -1,12 +1,5 @@
 package pl.michal.olszewski.rssaggregator.blog;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-
-import java.util.concurrent.Executors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,65 +8,78 @@ import org.springframework.test.util.ReflectionTestUtils;
 import pl.michal.olszewski.rssaggregator.extenstions.MockitoExtension;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.Collections;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UpdateBlogServiceTest {
 
-  private UpdateBlogService updateBlogService;
+    private UpdateBlogService updateBlogService;
 
-  @Mock
-  private BlogReactiveRepository blogRepository;
-  @Mock
-  private AsyncService asyncService;
+    @Mock
+    private BlogReactiveRepository blogRepository;
+    @Mock
+    private AsyncService asyncService;
 
-  @BeforeEach
-  void setUp() {
-    updateBlogService = new UpdateBlogService(blogRepository, asyncService, Executors.newSingleThreadExecutor());
-  }
+    @BeforeEach
+    void setUp() {
+        updateBlogService = new UpdateBlogService(blogRepository, asyncService, Executors.newSingleThreadExecutor());
+    }
 
-  @Test
-  void shouldNotUpdateBlogFromId() {
-    //given
-    given(blogRepository.findById("1")).willReturn(Mono.empty());
-    //when
-    //then
-    assertThatThrownBy(() -> updateBlogService.refreshBlogFromId("1")).hasMessageContaining("Nie znaleziono bloga");
-  }
+    @Test
+    void shouldNotUpdateBlogFromId() {
+        //given
+        given(blogRepository.findById("1")).willReturn(Mono.empty());
+        //when
+        //then
+        assertThatThrownBy(() -> updateBlogService.refreshBlogFromId("1")).hasMessageContaining("Nie znaleziono bloga");
+    }
 
-  @Test
-  void shouldUpdateBlogFromId() {
-    //given
-    given(blogRepository.findById("1")).willReturn(Mono.just(new Blog()));
-    //when
-    updateBlogService.refreshBlogFromId("1");
+    @Test
+    void shouldUpdateBlogFromId() {
+        //given
+        given(blogRepository.findById("1")).willReturn(Mono.just(new Blog()));
+        //when
+        updateBlogService.refreshBlogFromId("1");
 
-    verify(blogRepository, times(1)).findById("1");
-    verify(asyncService, times(1)).updateBlog(new Blog());
-    verifyNoMoreInteractions(blogRepository);
-  }
+        verify(blogRepository, times(1)).findById("1");
+        verify(asyncService, times(1)).updateBlog(new Blog());
+        verifyNoMoreInteractions(blogRepository);
+    }
 
-  @Test
-  void shouldRunUpdatesForAllBlogs() {
-    //given
-    ReflectionTestUtils.setField(updateBlogService, "enableJob", true);
-    given(blogRepository.findAll()).willReturn(Flux.just(new Blog()));
-    //when
-    updateBlogService.updatesBlogs();
+    @Test
+    void shouldRunUpdatesForAllBlogs() {
+        //given
+        ReflectionTestUtils.setField(updateBlogService, "enableJob", true);
+        given(blogRepository.findAll()).willReturn(Flux.just(new Blog()));
+        //when
+        StepVerifier.create(updateBlogService.updateAllActiveBlogs())
+            .expectNext(Collections.singletonList(Collections.singletonList(false)))
+            .expectComplete()
+            .verify();
 
-    verify(blogRepository, times(1)).findAll();
-    verify(asyncService, times(1)).updateBlog(new Blog());
-    verifyNoMoreInteractions(blogRepository);
-  }
+        verify(blogRepository, times(1)).findAll();
+        verify(asyncService, times(1)).updateBlog(new Blog());
+        verifyNoMoreInteractions(blogRepository);
+    }
 
-  @Test
-  void shouldNotRunUpdatesWhenIsNotEnabled() {
-    //given
-    ReflectionTestUtils.setField(updateBlogService, "enableJob", false);
-    //when
-    updateBlogService.updatesBlogs();
+    @Test
+    void shouldNotRunUpdatesWhenIsNotEnabled() {
+        //given
+        ReflectionTestUtils.setField(updateBlogService, "enableJob", false);
+        //when
+        StepVerifier.create(updateBlogService.updateAllActiveBlogs())
+            .expectComplete()
+            .verify();
 
-    verify(blogRepository, times(0)).findAll();
-    verify(asyncService, times(0)).updateBlog(new Blog());
-    verifyNoMoreInteractions(blogRepository);
-  }
+        verify(blogRepository, times(0)).findAll();
+        verify(asyncService, times(0)).updateBlog(new Blog());
+        verifyNoMoreInteractions(blogRepository);
+    }
 }
