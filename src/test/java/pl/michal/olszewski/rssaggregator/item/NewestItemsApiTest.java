9883e7b3988606d0324d9e2f698899687d1f484e
olszewskimichal <olszewskimichal@outlook.com>
2019-05-21 21:26:25 +0200

    dodanie paginacji do wyników zapytań

--- src/test/java/pl/michal/olszewski/rssaggregator/item/NewestItemsApiTest.java
+++ src/test/java/pl/michal/olszewski/rssaggregator/item/NewestItemsApiTest.java
@@ -50,7 +50,57 @@ class NewestItemsApiTest extends IntegrationTestBase {
     givenItem()
         .buildNumberOfItemsAndSave(6);
 
-    ListBodySpec<ItemDTO> itemDTOS = thenGetNumberItemsFromApi(3);
+    ListBodySpec<ItemDTO> itemDTOS = thenGetNumberItemsFromApiWithLimit(3);
+
+    itemDTOS.hasSize(3);
+  }
+
+  @Test
+  void should_get_second_page_of_items() {
+    givenItem()
+        .buildNumberOfItemsAndSave(6);
+
+    ListBodySpec<ItemDTO> itemDTOS = thenGetNumberItemsFromApiWithLimitAndPage(3, 2);
+
+    itemDTOS.hasSize(3);
+  }
+
+  @Test
+  void should_get_empty_list_of_items_sorted_by_createdAt() {
+    givenItem()
+        .buildNumberOfItemsAndSave(0);
+
+    ListBodySpec<ItemDTO> items = thenGetItemsSortedByCreatedAtFromApi();
+
+    items.hasSize(0);
+  }
+
+  @Test
+  void should_get_all_items_sorted_by_createdAt() {
+    givenItem()
+        .buildNumberOfItemsAndSave(3);
+
+    ListBodySpec<ItemDTO> items = thenGetItemsSortedByCreatedAtFromApi();
+
+    items.hasSize(3);
+  }
+
+  @Test
+  void should_get_limit_three_items_sorted_by_createdAt() {
+    givenItem()
+        .buildNumberOfItemsAndSave(6);
+
+    ListBodySpec<ItemDTO> itemDTOS = thenGetItemsSortedByCreatedAtWithLimitFromApi(3);
+
+    itemDTOS.hasSize(3);
+  }
+
+  @Test
+  void should_get_second_page_of_items_sorted_by_createdAt() {
+    givenItem()
+        .buildNumberOfItemsAndSave(6);
+
+    ListBodySpec<ItemDTO> itemDTOS = thenGetItemsSortedByCreatedAtForPageAndLimitFromApi(3, 2);
 
     itemDTOS.hasSize(3);
   }
@@ -66,12 +116,39 @@ class NewestItemsApiTest extends IntegrationTestBase {
         .expectBodyList(ItemDTO.class);
   }
 
-  private ListBodySpec<ItemDTO> thenGetNumberItemsFromApi(int number) {
+  private ListBodySpec<ItemDTO> thenGetNumberItemsFromApiWithLimit(int number) {
     return webTestClient.get().uri("http://localhost:{port}/api/v1/items?limit={number}", port, number)
         .exchange()
         .expectStatus().isOk()
         .expectBodyList(ItemDTO.class);
   }
 
+  private ListBodySpec<ItemDTO> thenGetNumberItemsFromApiWithLimitAndPage(int number, int page) {
+    return webTestClient.get().uri("http://localhost:{port}/api/v1/items?limit={number}&page={page}", port, number, page)
+        .exchange()
+        .expectStatus().isOk()
+        .expectBodyList(ItemDTO.class);
+  }
+
+  private ListBodySpec<ItemDTO> thenGetItemsSortedByCreatedAtFromApi() {
+    return webTestClient.get().uri("http://localhost:{port}/api/v1/items/createdAt", port)
+        .exchange()
+        .expectStatus().isOk()
+        .expectBodyList(ItemDTO.class);
+  }
+
+  private ListBodySpec<ItemDTO> thenGetItemsSortedByCreatedAtWithLimitFromApi(int number) {
+    return webTestClient.get().uri("http://localhost:{port}/api/v1/items/createdAt?limit={number}", port, number)
+        .exchange()
+        .expectStatus().isOk()
+        .expectBodyList(ItemDTO.class);
+  }
+
+  private ListBodySpec<ItemDTO> thenGetItemsSortedByCreatedAtForPageAndLimitFromApi(int number, int page) {
+    return webTestClient.get().uri("http://localhost:{port}/api/v1/items/createdAt?limit={number}&page={page}", port, number, page)
+        .exchange()
+        .expectStatus().isOk()
+        .expectBodyList(ItemDTO.class);
+  }
 
 }
