f24a2b3311c5c8549c9312eaa411359216773cdd
Michal Olszewski <michal.a.olszewski@allegro.pl>
2019-06-06 08:42:30 +0200

    refactor

--- src/main/java/pl/michal/olszewski/rssaggregator/blog/RssExtractorService.java
+++ src/main/java/pl/michal/olszewski/rssaggregator/blog/RssExtractorService.java
@@ -11,14 +11,11 @@ import java.net.URL;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
 import java.time.Instant;
 import java.util.ArrayList;
 import javax.net.ssl.KeyManager;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -67,19 +64,4 @@ class RssExtractorService {
     }
   }
 
-  static class DefaultTrustManager implements X509TrustManager {
-
-    @Override
-    public void checkClientTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
-    }
-
-    @Override
-    public void checkServerTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
-    }
-
-    @Override
-    public X509Certificate[] getAcceptedIssuers() {
-      return null;
-    }
-  }
 }
