879565128b535704b44082e873a90b605c08a1fc
Michal Olszewski <michal.a.olszewski@allegro.pl>
2019-06-07 15:07:17 +0200

    usunac nadmiarowy hashCode i equals

--- src/main/java/pl/michal/olszewski/rssaggregator/events/blogs/activity/ChangeActivityBlogEvent.java
+++ src/main/java/pl/michal/olszewski/rssaggregator/events/blogs/activity/ChangeActivityBlogEvent.java
@@ -2,14 +2,13 @@ package pl.michal.olszewski.rssaggregator.events.blogs.activity;
 
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.Objects;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Document
-public class ChangeActivityBlogEvent implements Serializable {
+class ChangeActivityBlogEvent implements Serializable {
 
   private final String blogId;
   private final Instant occurredAt;
@@ -21,21 +20,4 @@ public class ChangeActivityBlogEvent implements Serializable {
     this.occurredAt = occurredAt;
   }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    ChangeActivityBlogEvent that = (ChangeActivityBlogEvent) o;
-    return Objects.equals(blogId, that.blogId) &&
-        Objects.equals(occurredAt, that.occurredAt);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(blogId, occurredAt);
-  }
 }
