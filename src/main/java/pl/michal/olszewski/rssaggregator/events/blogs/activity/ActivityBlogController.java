06512c87170af5ebb5120b6928fa56eec0ab8161
olszewskimichal <olszewskimichal@outlook.com>
2019-06-11 18:19:57 +0200

    add new controller

---
+++ src/main/java/pl/michal/olszewski/rssaggregator/events/blogs/activity/ActivityBlogController.java
@@ -0,0 +1,35 @@
+package pl.michal.olszewski.rssaggregator.events.blogs.activity;
+
+import java.time.Instant;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/v1/blogs")
+@Slf4j
+public class ActivityBlogController {
+
+  private final BlogActivityEventProducer eventProducer;
+
+  public ActivityBlogController(BlogActivityEventProducer eventProducer) {
+    this.eventProducer = eventProducer;
+  }
+
+  @PostMapping(value = "/enable/{id}")
+  Mono<Boolean> enableBlogById(@PathVariable("id") String blogId) {
+    log.debug("Enable blog by Id {} ", blogId);
+    eventProducer.writeEventToQueue(ActivateBlog.builder().blogId(blogId).occurredAt(Instant.now()).build());
+    return Mono.just(Boolean.TRUE);
+  }
+
+  @PostMapping(value = "/disable/{id}")
+  Mono<Boolean> disableBlogById(@PathVariable("id") String blogId) {
+    log.debug("Disable blog by Id {} ", blogId);
+    eventProducer.writeEventToQueue(DeactivateBlog.builder().blogId(blogId).occurredAt(Instant.now()).build());
+    return Mono.just(Boolean.TRUE);
+  }
+}
