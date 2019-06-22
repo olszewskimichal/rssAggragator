7ac880781c03ed80df2c23ec5c90df549ee5af43
olszewskimichal <olszewskimichal@outlook.com>
2019-06-22 18:14:30 +0200

    szukanie tekstowe po zawartosci wpisow

--- model/src/main/java/pl/michal/olszewski/rssaggregator/item/Item.java
+++ model/src/main/java/pl/michal/olszewski/rssaggregator/item/Item.java
@@ -9,6 +9,7 @@ import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.index.TextIndexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -20,7 +21,9 @@ public final class Item {
 
   @Id
   private String id;
+  @TextIndexed(weight = 2)
   private String title;
+  @TextIndexed(weight = 1)
   private String description;
   @Indexed(unique = true)
   private String link;
