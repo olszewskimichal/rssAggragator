95ce51ef891e59b5d5403f763944e8731b1c6507
olszewskimichal <olszewskimichal@outlook.com>
2019-08-07 20:13:31 +0200

    add serializable

--- model/src/main/java/pl/michal/olszewski/rssaggregator/item/ItemDTO.java
+++ model/src/main/java/pl/michal/olszewski/rssaggregator/item/ItemDTO.java
@@ -3,6 +3,7 @@ package pl.michal.olszewski.rssaggregator.item;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.io.Serializable;
 import java.time.Instant;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,7 +11,7 @@ import lombok.Getter;
 @Getter
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class ItemDTO {
+public class ItemDTO implements Serializable {
 
   private final String title;
   private final String description;
