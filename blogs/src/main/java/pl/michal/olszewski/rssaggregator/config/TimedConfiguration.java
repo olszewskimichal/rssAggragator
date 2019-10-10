8722ba518ce8da174d0460c162dac81f2109fc65
olszewskimichal <olszewskimichal@outlook.com>
2019-10-10 19:39:58 +0200

    Fix for @Timed

---
+++ blogs/src/main/java/pl/michal/olszewski/rssaggregator/config/TimedConfiguration.java
@@ -0,0 +1,15 @@
+package pl.michal.olszewski.rssaggregator.config;
+
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TimedConfiguration {
+
+  @Bean
+  public TimedAspect timedAspect(MeterRegistry registry) {
+    return new TimedAspect(registry);
+  }
+}
